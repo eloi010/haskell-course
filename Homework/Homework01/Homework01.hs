9f1c315bc74875a1ce9ec53b40da1ee9a2232326
Eloi Manuel <eloi010@users.noreply.github.com>
2022-09-15 18:59:24 +0200

    Update Homework week 1
    Update Homework week 1

--- Homework/Homework01/Homework01.hs
+++ Homework/Homework01/Homework01.hs
@@ -1,16 +1,22 @@
 
 -- Question 1
 -- Write a multiline comment below.
+{-
+ test
+-}
 
 -- Question 2
 -- Define a function that takes a value and multiplies it by 3.
+mul3 x = x * 3
 
 -- Question 3
 -- Define a function that calculates the area of a circle.
+areaCircle x = pi * (x*x)
 
 -- Question 4
 -- Define a function that calculates the volume of a cylinder by composing the previous function together with the height of the cylinder. 
+volumeCylinder x y = (areaCircle x) * y
 
 -- Question 5
 -- Define a function that checks if the volume of a cylinder is greater than or equal to 42.
-
+greaterThan42 x y = if volumeCylinder x y >= 42 then "GT 42" else "LT 42"
